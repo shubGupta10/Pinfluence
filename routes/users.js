4078379c393498c696edc70ff65e29e0d1b1d385
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-03-17 23:18:51 +0530

    New Commit

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:27017/Pinfluence");
+// mongoose.connect("mongodb://127.0.0.1:27017/Pinfluence");
 const plm = require("passport-local-mongoose");
 
 const userSchema = new mongoose.Schema({
